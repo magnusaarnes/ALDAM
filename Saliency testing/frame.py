257898dd6932f213a29b9a02e1dbdc71662b0f7a
Eivind Dogger <eivind.dogger@gmail.com>
2022-03-23 12:44:23 +0100

    Working on stuff

--- Saliency testing/frame.py
+++ Saliency testing/frame.py
@@ -1,87 +1,21 @@
 import cv2
-from matplotlib import image
 import numpy as np
-
-
-def remove_specularity(img):
-    # convert to gray
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
-    #GLARE_MIN = np.array([180//2, 0, 0],np.uint8)
-    #GLARE_MAX = np.array([280//2, 0, 200],np.uint8)
-    
-    #frame_threshold = cv2.inRange(hsv_img, GLARE_MIN, GLARE_MAX)
-    #print("SE HER")
-    #print(np.min(frame_threshold), np.max(frame_threshold))
-    #mask = 255 - frame_threshold
-    
-    # Reduce noise in the saturation channel
-    hsv_img[:,:,1] = cv2.blur(hsv_img[:,:,1], (35,35), 0)
-    
-    # threshold grayscale image to extract glare
-    mask = cv2.threshold(hsv_img[:,:,2], 220, 255, cv2.THRESH_BINARY)[1]
-    kernel = np.ones((5,5),np.uint8)
-    mask = cv2.dilate(mask,kernel,iterations = 1)
-
-    # Optionally add some morphology close and open, if desired
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
-
-    # use mask with input to do inpainting
-    result = cv2.inpaint(img, mask, 21, cv2.INPAINT_TELEA) 
-
-    # display it
-    cv2.imshow("IMAGE", img)
-    cv2.imshow("GRAY", gray)
-    cv2.imshow("MASK", mask)
-    cv2.imshow("RESULT", result)
-    
-    return result
+from common import *
 
 
 def process_image(image, display_steps=False):
-    # Remove specular reflections
-    image = remove_specularity(image).copy()
-    
-    saliency = cv2.saliency.StaticSaliencyFineGrained_create()
-
-    # Do the saliency
-    success, image_saliency_map = saliency.computeSaliency(image)
-    if not success:
-        print("[ERROR] Saliency failed")
-        return
-    
-    saliency_map_image = (image_saliency_map * 255).astype("uint8")
-
-    saliency_map_image_bin = saliency_map_image > 60
-    saliency_map_image_bin = saliency_map_image_bin.astype("uint8") * 255
-
-    saliency_map_image_bin_blur = cv2.blur(saliency_map_image_bin, (23,23), 0)
-
-    saliency_map_image_bin_blur_bin = saliency_map_image_bin_blur > 10
-    saliency_map_image_bin_blur_bin = saliency_map_image_bin_blur_bin.astype("uint8") * 255
-    
-    if display_steps:
-        # Display original image
-        cv2.imshow('image', image)
-        cv2.imshow('saliency', saliency_map_image)
-        cv2.imshow('saliency_bin', saliency_map_image_bin)
-        cv2.imshow('saliency_bin_blur', saliency_map_image_bin_blur)
-        cv2.imshow('saliency_bin_blur_bin', saliency_map_image_bin_blur_bin)
-    
-    return saliency_map_image_bin_blur_bin
+    # Cook some code from Sindre!
+    return image
 
 
 class Frame:
-    instrinsic_params = np.loadtxt('config/intrinsic_params.txt')
+    K = np.loadtxt('config/intrinsic_params.txt')
+    dist_coeffs = np.loadtxt('config/dist_coeffs.txt')
     dist_coeffs = np.loadtxt('config/dist_coeffs.txt')
     width, height = np.loadtxt('config/image_dims.txt', dtype='int64')
+    #pixel_density = np.array([[1.55e-6], [1.55e-6]]) # From specs. Probably not needed
     new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(
-        instrinsic_params,
+        K,
         dist_coeffs,
         (width, height),
         1,
@@ -103,7 +37,10 @@ class Frame:
         - timestamp (float):          Unix timestamp of when image is taken.
         - orientation (float):        Orientation of camera in terms of [roll, pitch, yaw].
         """
-        self.image = image #cv2.undistort(image, self.instrinsic_params, self.dist_coeffs, None, self.new_camera_mtx)
+        self.image = image #cv2.undistort(image, self.K, self.dist_coeffs, None, self.new_camera_mtx)
+        #x, y, w, h = self.roi
+        #self.image = self.image[y:y+h, x:x+w]
+        #self.width, self.height = w, h
         self.center_coord = center_coord
         self.height_above_sea = height_above_sea
         self.timestamp = timestamp
@@ -111,15 +48,52 @@ class Frame:
     
     def process(self):
         self.image_processed = process_image(self.image, display_steps=True)
-        print(self.image_processed.shape)
         return self.image_processed
     
-    def get_centroids(self):
-        contours, _ = cv2.findContours(self.image_processed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        self.centroids = np.zeros((2, len(contours)))
-        self.contours = list(contours)
-        for i in range(len(self.contours)):
-            self.contours[i] = np.squeeze(self.contours[i], axis=1)
-            self.centroids[:,i] = np.mean(self.contours[i], axis=0)
+    def find_centroids(self):
+        #contours, _ = cv2.findContours(self.image_processed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #self.centroids = np.zeros((2, len(contours)))
+        #self.contours = list(contours)
+        #for i in range(len(self.contours)):
+        #    self.contours[i] = np.squeeze(self.contours[i], axis=1)
+        #    self.centroids[:,i] = np.mean(self.contours[i], axis=0)
+        
+        self.centroids = np.loadtxt('Positions/image1.txt').T
         
         return self.centroids
+    
+    def find_camera_coords(self):
+        # focal lengths and principal point (~center)
+        fx_pixels = self.K[0,0]
+        fy_pixels = self.K[1,1]
+        f = np.array([[fx_pixels], [fy_pixels]])
+        print("Focal lengths", f)
+        cx = self.K[0,2]
+        cy = self.K[1,2]
+        principal_point = np.array([[cx], [cy]])
+        print("Principal point:", principal_point)
+        n_points = self.centroids.shape[1]
+        self.Xc = np.empty((3,n_points))
+        
+        # Camera coord Z is aproximately equal height aboe sea level
+        Zc = self.height_above_sea
+        print("Height: ", Zc)
+        print("Centroids - CxCy:\n", (self.centroids - principal_point)[:,:4])
+        self.Xc[:2,:] = Zc * (self.centroids - principal_point) / f
+        self.Xc[2,:] = Zc
+        print(self.Xc[:,:4])
+        
+        return self.Xc
+    
+    def find_world_coords(self):
+        t_c_to_w = translate(150, 25, -self.height_above_sea)
+        R_c_to_w = rotate_z(np.deg2rad(90 + 90))
+        
+        T_c_to_w = t_c_to_w @ R_c_to_w
+        
+        n_points = self.Xc.shape[1]
+        Xc_tilde = np.column_stack((np.vstack((self.Xc, np.ones(n_points))), np.array([0,0,0,1])))
+        self.Xw = (T_c_to_w @ Xc_tilde)[:3,:]
+        
+        return self.Xw
+        
--- Saliency testing/testImages.py
+++ Saliency testing/testImages.py
@@ -5,15 +5,26 @@ from frame import Frame
     
 
 if __name__ == "__main__":
-    image = cv2.imread('images/image6.png')
+    image = cv2.imread('images/image1.png')
     
     img = Frame(image, [0.0, 0.0], 25, 0.0, [0, 0, 0])
     
     img.process()
     
-    centroids = img.get_centroids()
+    centroids = img.find_centroids()
+    Xc = img.find_camera_coords()
+    Xw = img.find_world_coords()
     
-    cv2.drawContours(img.image, img.contours, -1, (0,255,0), 2)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    #ax.scatter(Xc[0,:], Xc[1,:], Xc[2,:], marker='x')
+    ax.scatter(Xw[0,:], Xw[1,:], Xw[2,:], marker='x')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    
+    plt.figure()
+    #cv2.drawContours(img.image, img.contours, -1, (0,255,0), 2)
     #cv2.imshow('contours', img.image)
     plt.imshow(cv2.cvtColor(img.image, cv2.COLOR_BGR2RGB))
     plt.scatter(centroids[0,:], centroids[1,:], c="r", marker="x")
